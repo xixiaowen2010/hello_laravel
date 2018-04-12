25cb099f453965995ee4fef746475e4e9caec962
vagrant <vagrant@homestead>
2018-04-12 02:03:25 +0000

    update database config

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => $db_config['host'],
-            'port'     => env('DB_PORT', '5432'),
+            'port'     => '5432',
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
